1b2b2120e55218f9035320120e023a37163f57f2
Chris Holmes <j.chris.holmes@gmail.com>
2026-04-07 08:26:05 -0700

    Bump to v1.1.11

--- build_release.ps1
+++ build_release.ps1
@@ -11,7 +11,7 @@
 $ErrorActionPreference = "Stop"
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 $gameDir   = (Resolve-Path "$scriptDir\..\..").Path
-$version   = "1.1.10"
+$version   = "1.1.11"
 
 Write-Host ""
 Write-Host "=== Colony Spire Mod - Release Builder ===" -ForegroundColor Cyan
